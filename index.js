6edaa389dbbc5b34b24a77dcc396c9fd463a0cea
root <yluo3421@gmail.com>
2022-04-25 16:08:13 -0400

    commit tree 90a7b40133f24ebdd6116d503232094becc8b281

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(20, 1, 2, 4);
